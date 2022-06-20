8c52ad100aa455cc5e19c9d920f61bb5cb574ee5
enbrau <admin@enbrau.com>
2022-06-20 10:27:35 +0800

    fix mock server won't load

--- mock-server.js
+++ mock-server.js
@@ -3,44 +3,49 @@ const chalk = require('chalk')
 const bodyParser = require('body-parser')
 const requireContext = require('require-context')
 const path = require('path')
+const { mock } = require('mockjs')
 
 const mockDir = path.resolve(__dirname, './mock')
 
 function registerRoutes(app) {
-  const mocks = []
+  let mocks = []
   const moduleFiles = requireContext(mockDir, true, /\.js$/)
   moduleFiles.keys().forEach((key) => {
-    for (const mock of moduleFiles(key)) {
-      mocks.push(mock)
+    for (let apis of moduleFiles(key)) {
+      mocks = mocks.concat(apis)
     }
   })
 
+  console.log(mocks[1].url)
+
   let mockLastIndex = 0, mockRoutesLength = 0, mockStartIndex = 0
 
-  for (mock of mocks) {
-    app[mock.method](mock.url, (req, res) => {
-      // console.log('--->')
-      // res.header("Access-Control-Allow-Origin", "*");
-      // res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-      // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      // res.header('Access-Control-Allow-Headers', 'Content-Type');
-      // res.header("X-Powered-By",' 3.2.1')
-      // res.header("Content-Type", "application/json;charset=utf-8")
+  for (let i = 0; i < mocks.length; i++) {
+    let api = mocks[i]
+    console.log('Mock added: [' + api.method + ']' + api.url)
+    app[api.method](api.url, (req, res) => {
+      console.log('--->')
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header("X-Powered-By",' 3.2.1')
+      res.header("Content-Type", "application/json;charset=utf-8")
 
       try {
-        const response = mock.response()
+        const response = api.response()
         res.json(response)
       } catch(e) {
         console.error(e)
       }
     })
-
-    mockLastIndex    = app._router.stack.length
-    mockRoutesLength = Object.keys(mocks).length
-    mockStartIndex   = mockLastIndex - mockRoutesLength
-    
-    return { mockRoutesLength, mockStartIndex }
   }
+
+  mockLastIndex    = app._router.stack.length
+  mockRoutesLength = Object.keys(mocks).length
+  mockStartIndex   = mockLastIndex - mockRoutesLength
+  
+  return { mockRoutesLength, mockStartIndex }
 }
 
 module.exports = app => {
@@ -54,7 +59,7 @@ module.exports = app => {
   let mockStartIndex   = mockRoutes.mockStartIndex
 
   chokidar.watch(mockDir, {
-    ignored: /mock-server/,
+    ignored: /mock/,
     ignoreInitial: true
   }).on('all', (event, path) => {
     if (event === 'change' || event === 'add') {
