26eaf4e4f83dbd0746d12e82692a52d05180d737
enbrau <admin@enbrau.com>
2022-06-20 11:25:58 +0800

    fix mock server problem [HPM] Error occurred while proxying request [ECONNRESET]

--- webpack.config.js
+++ webpack.config.js
@@ -112,12 +112,23 @@ module.exports = {
     },
     proxy: {
       [process.env.CONTEXT_PATH]: {
+        static: __dirname + '/public/',
         target: process.env.HOST,
         changeOrigin: true,
         ws: true,
         secure: false,
         logLevel: 'debug',
-        pathRewrite: { '/mock': '' }
+        pathRewrite: { '/mock': '' },
+        onProxyReq:function (proxyReq, req, res, options) {
+          if (req.body) {
+            let bodyData = JSON.stringify(req.body);
+            // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+            proxyReq.setHeader('Content-Type','application/json');
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+            // stream the content
+            proxyReq.write(bodyData);
+          }
+        }
       }
     }
   }
